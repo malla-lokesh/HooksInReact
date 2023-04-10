583fc5549ce2b7376a8f46a48973a62dbd6a81c2
malla-lokesh <121810307046@gitam.in>
2023-04-10 20:33:44 +0530

    Completed Context API task

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,26 +1,31 @@
 import React from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContext from '../../Store/auth-context';
+import { useContext } from 'react';
 
 const Navigation = (props) => {
+
+  const context = useContext(AuthContext);
+
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {context.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
+          )}
+          {context.isLoggedIn && (
+            <li>
+              <a href="/">Admin</a>
+            </li>
+          )}
+          {context.isLoggedIn && (
+            <li>
+              <button onClick={context.onLogout}>Logout</button>
+            </li>
+          )}
       </ul>
     </nav>
   );
